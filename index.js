18951a95a4d89ee37646634a4862f17d3a0cbca4
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-03-04 23:50:36 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const databaseController = require("./src/controllers/databaseContro");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const storesRouter = require("./src/routes/storesRouter");
